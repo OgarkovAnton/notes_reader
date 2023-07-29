a0cd3161282630075f8593f30e3ce82bb9eeda8d
anton <oga@smena.space>
2023-07-29 11:44:31 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Notes Reader reads handwritten notes and return MIDI file with recognited song',
+    author='Anton',
+    license='',
+)
